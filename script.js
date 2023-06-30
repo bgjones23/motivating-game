47010dc4d959fcf589dbff24e9fa89563936c464
bgjones23 <68700318+bgjones23@users.noreply.github.com>
2023-06-30 13:04:17 -0400

    Update script.js
    Updating for more user-friendly intro and make version mobile.

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
     var canvas = document.getElementById("gameCanvas");
-    var ctx = canvas.getContext("2d");
+    canvas.width = 360;
+    canvas.height = 640;
+    var ctx = canvas.getContext("2d");    
 
-    var player = { x: 50, y: canvas.height - 30, size: 20, dy: 0 };
+    var player = {
+        x: 50,
+        y: canvas.height - 65,
+        size: 20,
+        dy: 2
+    };
+
     var obstacles = [];
     var gameOver = false;
-    var gravity = 1;
+    var gameStarted = false;
 
     var motivationalMessages = [
-        "Motivate", "Keep after it", "You got this", "Don't stop now", "Believe",
-        "You can do it", "Move faster next time", "Let's go", "Let's gooo", "Let's goooooo"
+        "Motivate",
+        "Keep after it",
+        "You got this",
+        "Don't stop now",
+        "Believe",
+        "You can do it",
+        "Move faster next time",
+        "Let's go",
+        "Let's gooo",
+        "Let's goooooo"
     ];
+    
+    function startGame() {
+        gameStarted = true;
+        update();
+    }
 
     document.addEventListener("keydown", function(event) {
-        if (event.key === "ArrowRight") player.x += 10;
-        if (event.key === "ArrowLeft") player.x -= 10;
+        if (!gameStarted) {
+            startGame();
+        } else {
+            var newX = player.x;
+            if(event.key === "ArrowRight") newX += 10;
+            if(event.key === "ArrowLeft") newX -= 10;
+            
+            // Check if new position is within bounds
+            if (newX >= 0 && newX <= canvas.width - player.size) {
+                player.x = newX;
+            }
+        }
     });
 
     function spawnObstacle() {
         var size = 20;
         var x = Math.random() * (canvas.width - size);
         var y = 0;
-        obstacles.push({ x, y, size, dy: 0 });
+        obstacles.push({x, y, size});
     }
 
     function collisionDetected(rect1, rect2) {
@@ -33,19 +64,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function resetGame() {
         player.x = 50;
-        player.y = canvas.height - 30;
-        player.dy = 0;
+        player.y = canvas.height - 65;
         obstacles = [];
         gameOver = false;
-        requestAnimationFrame(update);
+        gameStarted = false;
+        update();
     }
 
     function update() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        if (!gameStarted) {
+            // Display the initial message
+            ctx.fillStyle = "black";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
+            ctx.fillText("Press any key to start", canvas.width / 2, canvas.height / 2);
+            ctx.fillText("Use arrow keys to move", canvas.width / 2, canvas.height / 2 + 30);
+            return;
+        }
+
         // Draw player
         ctx.fillStyle = "blue";
-        player.y += player.dy;
         ctx.fillRect(player.x, player.y, player.size, player.size);
 
         // Draw obstacles
@@ -53,31 +93,34 @@ document.addEventListener('DOMContentLoaded', function() {
         for (var i = 0; i < obstacles.length; i++) {
             var obs = obstacles[i];
             obs.y += 5;
-            obs.y += obs.dy;
             ctx.fillRect(obs.x, obs.y, obs.size, obs.size);
 
             // Check for collisions
             if (collisionDetected(player, obs)) {
                 gameOver = true;
-                player.dy = gravity;
-                obs.dy = gravity;
-
-                setTimeout(function() {
-                    ctx.fillStyle = "black";
-                    ctx.font = "40px Arial";
-                    ctx.textAlign = "center";
-                    var randomMessage = motivationalMessages[Math.floor(Math.random() * motivationalMessages.length)];
-                    ctx.fillText(randomMessage, canvas.width / 2, canvas.height / 2);
-                }, 1000);
-
+                
+                // Display a random motivational message
+                ctx.fillStyle = "black";
+                ctx.font = "40px Arial";
+                ctx.textAlign = "center";
+                var randomMessage = motivationalMessages[Math.floor(Math.random() * motivationalMessages.length)];
+                ctx.fillText(randomMessage, canvas.width / 2, canvas.height / 2);
+                
+                // Reset the game after 2 seconds
                 setTimeout(resetGame, 2000);
                 return;
             }
         }
 
+        // Spawn new obstacles
         if (Math.random() < 0.05) spawnObstacle();
-        if (!gameOver) requestAnimationFrame(update);
+
+        // Request next animation frame
+        if (!gameOver) {
+            requestAnimationFrame(update);
+        }
     }
 
+    // Start the game loop
     update();
 });
