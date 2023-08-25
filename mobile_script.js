63f29154ecc126e7d1fcaddcf63519c72d55345a
bgjones23 <68700318+bgjones23@users.noreply.github.com>
2023-08-25 19:52:32 -0400

    Update mobile_script.js
    Even more mobile updates!

--- mobile_script.js
+++ mobile_script.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var ctx = canvas.getContext("2d");
 
     var player = {
-        x: 50,
+        x: canvas.width / 2,
         y: canvas.height - 25,
         size: 20,
-        dx: 5,
-        dy: 5
+        dx: 0,
+        dy: 0,
+        speed: 5
     };
 
     var obstacles = [];
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var gameClock = 0;
     var wave = 1;
     var gameStarted = false;
-    var moveDirection = null;
+    var joystickRadius = 30;
 
     var speedIncreaseFactor = 1.1; // 10% faster
     var obstacleSpawnRate = 0.05;
@@ -36,47 +37,42 @@ document.addEventListener('DOMContentLoaded', function() {
         "Let's goooooo"
     ];
 
-    document.addEventListener('touchstart', function(e) {
-        if (!gameStarted) {
-            gameStarted = true;
-            setInterval(updateGameClock, 1000);
-            update();
-        }
+    function calculateDirection(angle) {
+        var direction = { dx: 0, dy: 0 };
 
-        var touch = e.touches[0];
-        moveDirection = touch.clientX < canvas.width / 2 ? 'left' : 'right';
+        if (angle >= 45 && angle < 135) direction.dy = -1; // Up
+        else if (angle >= 135 && angle < 225) direction.dx = -1; // Left
+        else if (angle >= 225 && angle < 315) direction.dy = 1; // Down
+        else direction.dx = 1; // Right
 
-        // Check for up or down touch
-        if (touch.clientY < canvas.height / 2) {
-            moveDirection = 'up';
-        } else {
-            moveDirection = 'down';
-        }
-    });
+        return direction;
+    }
 
-    document.addEventListener('touchend', function(e) {
-        moveDirection = null;
-    });
+    function getAngle(x, y) {
+        return (Math.atan2(y, x) * 180) / Math.PI;
+    }
 
     function spawnObstacle() {
         var size = 20;
         var x = Math.random() * (canvas.width - size);
         var y = 0;
-        obstacles.push({x, y, size});
+        obstacles.push({ x, y, size });
     }
 
     function spawnPurpleObstacle() {
         var size = 20;
         var x = Math.random() * (canvas.width - size);
         var y = 0;
-        obstacles.push({x, y, size, color: "purple"});
+        obstacles.push({ x, y, size, color: "purple" });
     }
 
     function collisionDetected(rect1, rect2) {
-        return rect1.x < rect2.x + rect2.size &&
-               rect1.x + rect1.size > rect2.x &&
-               rect1.y < rect2.y + rect2.size &&
-               rect1.y + rect1.size > rect2.y;
+        return (
+            rect1.x < rect2.x + rect2.size &&
+            rect1.x + rect1.size > rect2.x &&
+            rect1.y < rect2.y + rect2.size &&
+            rect1.y + rect1.size > rect2.y
+        );
     }
 
     function updateGameClock() {
@@ -92,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function resetGame() {
-        player.x = 50;
+        player.x = canvas.width / 2;
         player.y = canvas.height - 25;
         obstacles = [];
         timer = 100;
@@ -109,21 +105,34 @@ document.addEventListener('DOMContentLoaded', function() {
             ctx.font = "20px Arial";
             ctx.textAlign = "center";
             ctx.fillText("Tap to start", canvas.width / 2, canvas.height / 2);
-            ctx.fillText("Tap left, right, up, or down to move", canvas.width / 2, canvas.height / 2 + 30);
+            ctx.fillText(
+                "Move the joystick to control the blue square",
+                canvas.width / 2,
+                canvas.height / 2 + 30
+            );
             return;
         }
 
+        var joystickX = player.x + player.size / 2 + player.dx * joystickRadius;
+        var joystickY = player.y + player.size / 2 + player.dy * joystickRadius;
+
+        // Draw joystick
+        ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+        ctx.beginPath();
+        ctx.arc(joystickX, joystickY, joystickRadius, 0, Math.PI * 2);
+        ctx.fill();
+
         // Move player
-        if (moveDirection === 'left') player.x -= player.dx;
-        if (moveDirection === 'right') player.x += player.dx;
-        if (moveDirection === 'up') player.y -= player.dy;
-        if (moveDirection === 'down') player.y += player.dy;
+        player.x += player.dx * player.speed;
+        player.y += player.dy * player.speed;
 
         // Keep player within canvas
         if (player.x < 0) player.x = 0;
-        if (player.x + player.size > canvas.width) player.x = canvas.width - player.size;
+        if (player.x + player.size > canvas.width)
+            player.x = canvas.width - player.size;
         if (player.y < 0) player.y = 0;
-        if (player.y + player.size > canvas.height) player.y = canvas.height - player.size;
+        if (player.y + player.size > canvas.height)
+            player.y = canvas.height - player.size;
 
         // Draw player
         ctx.fillStyle = "blue";
@@ -147,8 +156,17 @@ document.addEventListener('DOMContentLoaded', function() {
                     ctx.fillStyle = "black";
                     ctx.font = "30px Arial";
                     ctx.textAlign = "center";
-                    var randomMessage = motivationalMessages[Math.floor(Math.random() * motivationalMessages.length)];
-                    ctx.fillText(randomMessage, canvas.width / 2, canvas.height / 2);
+                    var randomMessage =
+                        motivationalMessages[
+                            Math.floor(
+                                Math.random() * motivationalMessages.length
+                            )
+                        ];
+                    ctx.fillText(
+                        randomMessage,
+                        canvas.width / 2,
+                        canvas.height / 2
+                    );
 
                     // Reset the game after 2 seconds
                     setTimeout(resetGame, 2000);
@@ -173,10 +191,13 @@ document.addEventListener('DOMContentLoaded', function() {
         ctx.fillStyle = "white";
         ctx.fillRect(canvas.width - 120, 10, 110, 30);
         ctx.fillStyle = "black";
-        ctx.font = "18px Arial";
+        ctx.font = "20px Arial";
         ctx.textAlign = "right";
-        ctx.fillText(`Time: ${timer}s`, canvas.width - 15, 30);
-        ctx.fillText(`Wave: ${wave}`, canvas.width - 15, 50);
+        ctx.fillText("Time: " + timer + "s", canvas.width - 10, 30);
+        ctx.fillText("Wave: " + wave, canvas.width - 10, 60);
+
+        // Update game clock
+        updateGameClock();
 
         // Request next animation frame
         requestAnimationFrame(update);
