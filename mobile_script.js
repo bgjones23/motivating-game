9c5c0b2d279a7fc8da5c84097e01f924c0813072
bgjones23 <68700318+bgjones23@users.noreply.github.com>
2023-08-26 08:51:12 -0400

    Update mobile_script.js

--- mobile_script.js
+++ mobile_script.js
@@ -109,7 +109,7 @@ canvas.addEventListener('touchstart', function(event) {
         touchInterval = setInterval(function() {
             player.x += player.dx;
             player.x = Math.max(0, Math.min(canvas.width - player.size, player.x));
-        }, 16); // Adjust the interval as needed (e.g., 32ms for 60 FPS)
+        }, 32); // Adjust the interval as needed (e.g., 64ms for 60 FPS)
     }
 });
 
