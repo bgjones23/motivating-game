5650ede184b8da35e9fbd2de800582ee95a4aff7
bgjones23 <68700318+bgjones23@users.noreply.github.com>
2023-08-26 02:53:22 -0400

    Update mobile_script.js
    Hrmph!

--- mobile_script.js
+++ mobile_script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var canvas = document.getElementById("gameCanvas");
     canvas.width = window.innerWidth - 10;
     canvas.height = window.innerHeight - 20;
@@ -9,17 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var gameOver = false, gameStarted = false;
     var touchStartPosition = null, touchMoveDirection = null;
 
-    canvas.addEventListener('touchstart', function() {
-        if (!gameStarted) { gameStarted = true; update(); }
+    canvas.addEventListener('touchstart', function (event) {
+        if (!gameStarted) {
+            gameStarted = true;
+            update();
+        }
         touchStartPosition = event.touches[0].clientX;
     });
 
-    canvas.addEventListener('touchmove', function() {
+    canvas.addEventListener('touchmove', function (event) {
         if (!touchStartPosition) return;
         touchMoveDirection = event.touches[0].clientX < touchStartPosition ? 'left' : 'right';
     });
 
-    canvas.addEventListener('touchend', function() {
+    canvas.addEventListener('touchend', function () {
         touchStartPosition = touchMoveDirection = null;
     });
 
@@ -32,36 +35,58 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function resetGame() {
-        player.x = canvas.width / 2 - 10; player.y = canvas.height - 25; obstacles = [];
-        gameOver = false; gameStarted = false;
+        player.x = canvas.width / 2 - 10;
+        player.y = canvas.height - 25;
+        obstacles = [];
+        gameOver = false;
+        gameStarted = false;
         setTimeout(update, 500);
     }
 
     function update() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         if (!gameStarted) {
-            ctx.fillStyle = "black"; ctx.font = "20px Arial"; ctx.textAlign = "center";
+            ctx.fillStyle = "black";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
             ctx.fillText("Tap to start", canvas.width / 2, canvas.height / 2);
             return;
         }
+
         if (touchMoveDirection === 'left') player.x -= player.dx;
         if (touchMoveDirection === 'right') player.x += player.dx;
         player.x = Math.max(0, Math.min(canvas.width - player.size, player.x));
-        ctx.fillStyle = "blue"; ctx.fillRect(player.x, player.y, player.size, player.size);
+
+        ctx.fillStyle = "blue";
+        ctx.fillRect(player.x, player.y, player.size, player.size);
+
         ctx.fillStyle = "red";
         for (var i = 0; i < obstacles.length; i++) {
             var obs = obstacles[i];
-            obs.y += 5; ctx.fillRect(obs.x, obs.y, obs.size, obs.size);
+            obs.y += 5;
+            ctx.fillRect(obs.x, obs.y, obs.size, obs.size);
             if (collisionDetected(player, obs)) {
-                gameOver = true; ctx.fillStyle = "black"; ctx.font = "30px Arial"; ctx.textAlign = "center";
+                gameOver = true;
+                ctx.fillStyle = "black";
+                ctx.font = "30px Arial";
+                ctx.textAlign = "center";
                 ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2);
-                setTimeout(resetGame, 2000); return;
+                setTimeout(resetGame, 2000);
+                return;
             }
         }
-        if (Math.random() < 0.1) spawnObstacle(); // Adjust obstacle spawning
-        ctx.fillStyle = "black"; ctx.font = "14px Arial"; ctx.textAlign = "right";
+
+        if (Math.random() < 0.1) spawnObstacle();
+
+        ctx.fillStyle = "black";
+        ctx.font = "14px Arial";
+        ctx.textAlign = "right";
         ctx.fillText(`Score: ${obstacles.length}`, canvas.width - 10, 20);
-        if (!gameOver) requestAnimationFrame(update);
+
+        if (!gameOver) {
+            requestAnimationFrame(update);
+        }
     }
 
     update();
